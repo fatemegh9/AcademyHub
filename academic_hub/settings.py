1679cd35a801eb082076fb6fdf09914c522855ac
FatemeQr <fateme.ghorbani2005@gmail.com>
2026-07-09 17:05:07 +0330

    Update Django storage settings2

--- academic_hub/settings.py
+++ academic_hub/settings.py
@@ -57,7 +57,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
